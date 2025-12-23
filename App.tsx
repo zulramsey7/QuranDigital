6233d8b7979d3c2356b35c84d554053a546ab78b
zulramsey7 <zulfikarazizul7@gmail.com>
2025-12-23 22:10:36 +0800

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,56 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LanguageProvider } from "@/contexts/LanguageContext";
+import HomePage from "./pages/HomePage";
+import QuranPage from "./pages/QuranPage";
+import QiblaPage from "./pages/QiblaPage";
+import DoaPage from "./pages/DoaPage";
+import TasbihPage from "./pages/TasbihPage";
+import ProfilePage from "./pages/ProfilePage";
+import SettingsPage from "./pages/SettingsPage";
+import NotFound from "./pages/NotFound";
+import TahlilRingkas from "./pages/TahlilRingkas";
+import TahlilLengkap from "./pages/TahlilLengkap";
+import YasinPage from "./pages/Yasin"; 
+import SirahPage from "./pages/Sirah"; 
+import InstallPWA from "./InstallPWA"; // Import komponen baru anda
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <LanguageProvider>
+      <TooltipProvider>
+        {/* Notifikasi PWA diletakkan di sini supaya muncul di semua page */}
+        <InstallPWA /> 
+        
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/quran" element={<QuranPage />} />
+            <Route path="/qibla" element={<QiblaPage />} />
+            <Route path="/kiblat" element={<QiblaPage />} /> 
+            <Route path="/doa" element={<DoaPage />} />
+            <Route path="/tasbih" element={<TasbihPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/settings" element={<SettingsPage />} />
+            
+            <Route path="/yasin" element={<YasinPage />} />
+            <Route path="/tahlil-ringkas" element={<TahlilRingkas />} />
+            <Route path="/tahlil-lengkap" element={<TahlilLengkap />} />
+            <Route path="/sirah" element={<SirahPage />} /> 
+            
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </LanguageProvider>
+  </QueryClientProvider>
+);
+
+export default App;
