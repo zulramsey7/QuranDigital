825a0a6c39849ccbdef9ede12cd97563f096f5fa
zulramsey7 <zulfikarazizul7@gmail.com>
2025-12-27 13:44:53 +0800

    Update DoaPage.tsx

--- src/pages/DoaPage.tsx
+++ src/pages/DoaPage.tsx
@@ -17,7 +17,7 @@ const STATIC_DOAS: Doa[] = [
   {
     id: '0',
     doa: 'Doa Selamat',
-    ayat: 'اَللّٰهُمَّ اِنَّا نَسْئَلُكَ سَلَامَةً فِى الدِّيْنِ وَعَافِيَةً فِى الْجَسَدِ وَزِيَادَةً فِى الْعِلْمِ وَبَرَكَةً فِى الرِّزْقِ وَتَوْبَةً قَبْلَ الْمَوْتِ وَرَحْمَةً عِنْدَ الْمَوْتِ وَمَغْفِرَةً بَعْدَ الْمَوْتِ. اَللّٰهُمَّ هَوِّنْ عَلَيْنَا فِى سَكَرَاتِ الْمَوْتِ وَالنَّجَاةَ مِنَ النَّارِ وَالْعَفْوَ عِنْدَ الْحِسَابِ',
+    ayat: 'اَللّٰهُمَّ اِنَّا نَسْئَلُكَ سَلَامَةً فِى الدِّيْنِ وَعَافِيَةً فِى الْجَسَدِ وَزِيَادَةً فِى الْعِلْمِ وَبَرَكَةً فِى الرِّزْقِ وَتَوْبَةً قَبْلَ الْمَوْتِ وَرَحْمَةً عِنْدَ الْمَوْتِ وَمَغْفِرَةً بَعْدَ الْمَوْتِ ۖ اَللّٰهُمَّ هَوِّنْ عَلَيْنَا فِى سَكَرَاتِ الْمَوْتِ وَالنَّجَاةَ مِنَ النَّارِ وَالْعَفْوَ عِنْدَ الْحِسَابِ',
     latin: "Allahumma inna nas-aluka salamatan fiddin, wa 'afiyatan fil jasad, wa ziyadatan fil 'ilm, wa barakatan fir-rizq, wa taubatan qablal maut, wa rahmatan 'indal maut, wa maghfiratan ba'dal maut. Allahumma hawwin 'alaina fi sakaratil maut, wannajata minannari wal 'afwa 'indal hisab.",
     artinya: 'Ya Allah, sesungguhnya kami memohon kepada-Mu keselamatan dalam agama, kesihatan jasad, tambahan ilmu, keberkatan rezeki, taubat sebelum mati, rahmat ketika mati, dan keampunan selepas mati. Ya Allah, mudahkanlah kami dalam sakaratul maut, lepaskanlah kami dari api neraka dan berilah kemaafan ketika hari perhitungan.'
   },
